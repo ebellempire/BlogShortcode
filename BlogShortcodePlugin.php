44cc22f206f5eab0a7644ed4347681f9b11efe97
Erin Bell <campus@dyn-host-137-148-121-48.csuohio.edu>
2017-05-05 14:25:11 -0400

    initial commit

---
+++ BlogShortcodePlugin.php
@@ -0,0 +1,68 @@
+<?php
+class BlogShortcodePlugin extends Omeka_Plugin_AbstractPlugin
+{
+
+	protected $_hooks = array(
+		'initialize',
+	);
+
+
+	/**
+	 * Add Shortcode.
+	 */
+
+    public function hookInitialize()
+    {
+        add_shortcode('blog', array($this, 'bs_postlist'));
+    }
+    
+    public function bs_postlist($args)
+    {
+        return '<div id="bs-widget-container">'.bs_display_postlist($args).'</div>';
+    }	     
+	
+}	
+
+function bs_sortByDateDesc( $a, $b ) {
+    return strtotime($a["inserted"]) < strtotime($b["inserted"]);
+}
+
+function bs_display_postlist($args){
+	if($current=get_current_record('SimplePagesPage',false)){
+		$blogParentId=isset($args['parent']) && ($bp=get_record('SimplePagesPage',array('slug'=>$args['parent']))) ? 
+			$bp->id : $current->id;
+		$pages = get_db()->getTable('SimplePagesPage')->findAll();
+		$total=isset($args['number']) ? filter_var($args['number'],FILTER_VALIDATE_INT)  : 10;
+		$excerpt_length=isset($args['length']) ? filter_var($args['length'],FILTER_VALIDATE_INT) : 500; 
+		$show_author=isset($args['author']) ? filter_var($args['author'],FILTER_VALIDATE_BOOLEAN) : true;
+		$show_date=isset($args['date']) ? filter_var($args['date'],FILTER_VALIDATE_BOOLEAN) : true;
+		$posts=array();
+		$i=0;
+		usort($pages, "bs_sortByDateDesc");
+		foreach($pages as $page){
+			if($i==$total) break;
+			$class=(($i+1) % 2 == 0) ? 'even' : 'odd';
+			$html=null;
+			if($page['parent_id']==$blogParentId && $page['is_published']){
+				$date=date('d M Y',strtotime($page['inserted']));
+				$url=WEB_ROOT.'/'.$page['slug'];
+				$userid=$page['created_by_user_id']; 
+				$username=get_record_by_id('user',$userid)->name ? 
+					get_record_by_id('user',$userid)->name : 
+					get_record_by_id('user',$userid)->username;
+				$html.='<article class="post post-position-'.($i+1).' '.$class.'">';
+				$html.='<h3><a href="'.$url.'">'.$page['title'].'</a></h3>';
+				$html.='<div class="byline">'.($show_date ? __('Posted on %s',$date) : null).' '.($show_author ? __('by %s',$username) : null).'</div>';
+				$html.='<p>'.snippet($page['text'],0,$excerpt_length,
+					'&nbsp;&hellip; <a href="'.$url.'">'.__('Read More').'</a>').'</p>';
+				$html.='</article>';
+				
+				$posts[]=$html;
+				$i++;
+			}
+		}
+	return count($posts)>0 ? '<div class="posts">'.implode('',$posts) .'</div>' : '<p><em>No posts found!</em></p>';
+	}else{
+		return '<p><em>No posts found!</em></p>';
+	}
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Blog Shortcode
+Allows users to display a list of simple pages in a blog-like format.
+
+## Usage
+Create a simple page to use as a "blog" (for example, a page with the title of "News and Events" and the slug "news"). The slug is important because it will be the main way you control which posts appear. Create "blog posts" by adding new simple pages and organizing them as children of the chosen "blog" page (for instance, setting the "parent page" to "News and Events"). 
+
+## Options
+- **parent**: the slug of the parent page whose child pages should be displayed. *String* (default is `current page`)
+- **number**: the max. number of child pages to display. *Number* (default is `10`)
+- **length**: the length of the text snippet (excerpt) for each child page. *Number* (default is `500`)
+- **author**: sets whether to display the name of the user who created the page. *Boolean* (default is `true`)
+- **date**: sets whether to display the date the page was created. *Boolean* (default is `true`)
+
+## Example with no options
+`[blog]`
+Placing this on a simple page will display all children of that page as a blog-styled list, with the most recently-added at the top.
+
+## Example with all options
+`[blog parent="news" number="10" length="500" author="false" date="true"]`
+This example will diplay the *10* most recent children of the page that has the slug *news*. Each entry will display a *date* in the byline but not an *author* and the text snippet for each entry will be *500* characters long.
